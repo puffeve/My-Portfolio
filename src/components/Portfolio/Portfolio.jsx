f8ae0173e37f053598fe31a8b21460ee8df1ee31
puffeve <jiraphan389@gmail.com>
2024-03-04 19:41:01 +0700

    Ahhh

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,7 +7,7 @@ function Portfolio() {
       <h3 className={styles.port_title}>Portfolio</h3>
       <div className={styles.port_list}>
         <div className={styles.port_items}>
-            <img src="src/components/Photo/p1.jpg" alt="" />
+            <img src="src\components\Photo\p1.jpg" alt="" />
             <p>Project Title</p>
         </div>
         <div className={styles.port_items}>
