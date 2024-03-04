41316c49763ae57f9bdc06d07a340284836fc51e
puffeve <jiraphan389@gmail.com>
2024-03-04 20:11:44 +0700

    รอบสาม

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './Portfolio.module.css'
-import Photo from "./components/Photo"
  
 function Portfolio() {
   return (
@@ -8,7 +7,7 @@ function Portfolio() {
       <h3 className={styles.port_title}>Portfolio</h3>
       <div className={styles.port_list}>
         <div className={styles.port_items}>
-            <img src="./src/components/Photo/p1.jpg" alt="" />
+            <img src={"./src/components/Photo/p1.jpg"} alt="" />
             <p>Project Title</p>
         </div>
         <div className={styles.port_items}>
